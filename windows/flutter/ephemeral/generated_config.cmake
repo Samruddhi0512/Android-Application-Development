8e8d432886909dbebe4a3a8246ac3b7e06621f3d
Samruddhi Sunil Bate <samruddhibate05@gmail.com>
2026-02-11 22:57:32 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\samru\\Downloads\\flutter_windows_3.38.7-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter\\login_page1" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\samru\\Downloads\\flutter_windows_3.38.7-stable\\flutter"
+  "PROJECT_DIR=D:\\flutter\\login_page1"
+  "FLUTTER_ROOT=C:\\Users\\samru\\Downloads\\flutter_windows_3.38.7-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter\\login_page1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter\\login_page1"
+  "FLUTTER_TARGET=D:\\flutter\\login_page1\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049M2I2MmVmYzJhMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter\\login_page1\\.dart_tool\\package_config.json"
+)
