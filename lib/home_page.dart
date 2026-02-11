7c4cd2f45fe82f6c10576827600441ba71136832
Samruddhi Sunil Bate <samruddhibate05@gmail.com>
2026-02-11 22:50:30 +0530

    Add files via upload

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String userId;
+
+  const HomePage({super.key, required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Text(
+          "Welcome $userId",
+          style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:login_page1/utility.dart';
+import 'package:flutter/material.dart';
+import 'package:login_page1/register_page.dart';
+import 'package:login_page1/configurations.dart';
+import 'package:login_page1/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Login'),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _userID = TextEditingController(), _password = TextEditingController();
+  String? _userIDErrorText, _passwordErrorText;
+
+
+  void validate() {
+    setState(() {
+      _userIDErrorText = _setUserIDErrorText(_userID.text);
+      _passwordErrorText = _setPasswordErrorText(_password.text);
+    });
+
+    if (_userIDErrorText == null && _passwordErrorText == null) {
+      bool isValidUser = false;
+
+      for (var user in Configurations.credentials) {
+        if (user['userid'] == _userID.text &&
+            user['password'] == _password.text) {
+          isValidUser = true;
+          break;
+        }
+      }
+
+      if (isValidUser) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(userId: _userID.text),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Invalid User ID or Password")),
+        );
+      }
+    }
+  }
+
+
+
+  String? _setUserIDErrorText(String value) {
+    if (value.isEmpty) return 'Please enter email ID';
+    if (!Utility.validateEmail(value))
+      return 'Please enter valid email ID';
+    return null;
+  }
+
+
+  String? _setPasswordErrorText(String value) {
+    if (value.isEmpty) return 'Please enter Password';
+    return null;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ValueListenableBuilder<TextEditingValue>(
+                  valueListenable: _userID,
+                  builder: (context, value, child) {
+                    _userIDErrorText = _setUserIDErrorText(value.text);
+                    return TextField(
+                      controller: _userID,
+                      decoration: InputDecoration(
+                        labelText: 'User ID',
+                        errorText: _userIDErrorText,
+                        hintText: 'dhiraj.jadhav@vit.edu',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    );
+                  }
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                obscureText: true,
+                controller: _password,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  errorText: _passwordErrorText,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: validate, child: Text('Login')),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context,MaterialPageRoute(builder: (context)=>RegisterPage()
+              ));
+            }, child: Text('Sign Up')),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
